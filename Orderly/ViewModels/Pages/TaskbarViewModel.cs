af1c0c1c32f96275d63b8858e2d898e0e2545be3
George Patrut <g.patrut@maticafintec.com>
2024-02-09 09:42:58 +0100

    fixed taskbar not closing

--- Orderly/ViewModels/Pages/TaskbarViewModel.cs
+++ Orderly/ViewModels/Pages/TaskbarViewModel.cs
@@ -1,4 +1,6 @@
-﻿using Orderly.Views.Windows;
+﻿using Hardcodet.Wpf.TaskbarNotification;
+using Orderly.Views.Pages;
+using Orderly.Views.Windows;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,26 +11,36 @@ using Wpf.Ui.Controls;
 
 namespace Orderly.ViewModels.Pages
 {
-    public partial class TaskbarViewModel : ViewModelBase, INavigationAware
+    public partial class TaskbarViewModel : ViewModelBase
     {
-        public void OnNavigatedFrom()
-        {
-        }
-
-        public void OnNavigatedTo()
-        {
-        }
+        TaskbarIcon tb;
 
         [RelayCommand]
         public void OpenMainWindow()
         {
-            MainWindow.Instance.Show();
+            MainWindow.Instance.ShowWindowAgain();
+
+            if(tb == null) tb = (TaskbarIcon)Application.Current.FindResource("TaskBarIcon");
+            tb.TrayPopupResolved.IsOpen = false;
         }
 
         [RelayCommand]
         public void CloseApp()
         {
             Application.Current.Shutdown();
+
+            if (tb == null) tb = (TaskbarIcon)Application.Current.FindResource("TaskBarIcon");
+            tb.TrayPopupResolved.IsOpen = false;
+        }
+
+        [RelayCommand]
+        public void OpenSetting()
+        {
+            MainWindow.Instance.ShowWindowAgain();
+            MainWindow.Instance.Navigate(typeof(SettingsPage));
+
+            if (tb == null) tb = (TaskbarIcon)Application.Current.FindResource("TaskBarIcon");
+            tb.TrayPopupResolved.IsOpen = false;
         }
     }
 }
