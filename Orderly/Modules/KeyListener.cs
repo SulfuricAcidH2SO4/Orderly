13bf98bd1ec8914e6a9695c9d3089813db095a6a
George Patrut <ggeorge.patrut@gmail.com>
2024-02-14 19:12:34 +0100

    fixed some issues

--- Orderly/Modules/KeyListener.cs
+++ Orderly/Modules/KeyListener.cs
@@ -9,14 +9,20 @@ namespace Orderly.Modules
         [STAThread]
         public static void InitializeHook()
         {
-            var hook = new TaskPoolGlobalHook();
-            hook.KeyPressed += OnKeyPressed;
-            hook.Run();
+            Task.Factory.StartNew(() =>
+            {
+                var hook = new TaskPoolGlobalHook();
+                hook.KeyPressed += OnKeyPressed;
+                hook.Run();
+            });
         }
 
         private static void OnKeyPressed(object? sender, KeyboardHookEventArgs e)
         {
-            
+            if((e.RawEvent.Mask & (SharpHook.Native.ModifierMask.LeftAlt | SharpHook.Native.ModifierMask.LeftCtrl)) == (SharpHook.Native.ModifierMask.LeftAlt | SharpHook.Native.ModifierMask.LeftCtrl) && e.Data.KeyCode == SharpHook.Native.KeyCode.VcP)
+            {
+                MessageBox.Show("zaaaamn");
+            }
         }
     }
 }
