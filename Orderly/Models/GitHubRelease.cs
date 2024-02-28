f7abe523e4a61834522d5b424ac77f79fa512811
George Patrut <george.patrut@libero.it>
2024-02-28 14:02:38 +0100

    wip updater

---
+++ Orderly/Models/GitHubRelease.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orderly.Models
+{
+    public sealed class GitHubRelease
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; } = string.Empty;
+
+        [JsonProperty("url")]
+        public string Url { get; set; } = string.Empty;
+
+        [JsonProperty("name")]
+        public string Name { get; set; } = string.Empty;
+
+        [JsonProperty("zipball_url")]
+        public string ZipBallUrl { get; set; } = string.Empty;
+
+        [JsonIgnore]
+        public Version Version
+        {
+            get
+            {
+                try {
+                    Version v = new(Name.Replace("v", string.Empty));
+                    return v;
+                }
+                catch {
+                    return new Version("1.0.0");
+                }
+            }
+        }
+    }
+}
---
+++ Orderly/Update/ProgramUpdater.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Orderly.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Reflection;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orderly.Update
+{
+    public static class ProgramUpdater
+    {
+        public static bool CheckUpdate(out Version currentVersion, out Version latestVersion)
+        {
+            try {
+                Version v = Assembly.GetExecutingAssembly().GetName().Version!;
+                currentVersion = v;
+                string response = MakeGetRequest("https://api.github.com/repos/SulfuricAcidH2SO4/Orderly/releases");
+                List<GitHubRelease> releases = JsonConvert.DeserializeObject<List<GitHubRelease>>(response)!;
+
+                if (releases.Any(x => x.Version > v)) {
+                    latestVersion = releases.Max(x => x.Version)!;
+                    return true;
+                }
+                else {
+                    latestVersion = currentVersion;
+                    return false;
+                }
+            }
+            catch (Exception e) {
+                Version v = Assembly.GetExecutingAssembly().GetName().Version!;
+                currentVersion = v;
+                latestVersion = v;
+                return false;
+            }
+        }
+
+        static string MakeGetRequest(string url)
+        {
+            using (WebClient webClient = new()) {
+                webClient.Headers.Add(HttpRequestHeader.UserAgent, "Update-Checker");
+                return webClient.DownloadString(url);
+            }
+        }
+    }
+}
